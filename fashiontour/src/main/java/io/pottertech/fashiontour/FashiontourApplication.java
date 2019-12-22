2ba031167cf280a05fcc6d71e4ad63d621a3a27e
Athavan <p.adhavan71@gmail.com>
2019-12-22 19:34:35 +0530

    initial commit

---
+++ fashiontour/src/main/java/io/pottertech/fashiontour/FashiontourApplication.java
@@ -0,0 +1,13 @@
+package io.pottertech.fashiontour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashiontourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashiontourApplication.class, args);
+	}
+
+}
---
+++ fashiontour/src/test/java/io/pottertech/fashiontour/FashiontourApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pottertech.fashiontour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashiontourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
